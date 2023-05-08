fcc59649c6eea71fdf670bd84f880be78611ab72
PedroSimbera <pedrosimbera@hotmail.com>
2023-05-08 11:42:09 -0300

    sobre funções recursivas atualização

--- FuncoesRecursivas.py
+++ FuncoesRecursivas.py
@@ -10,3 +10,10 @@
 # Aqui o código irá realizar uma entrada, que irá diminuir o valor para que chegue ao 1. Fatorial.
 #
 # Após isso basta chamar a função com o valor que deseja fazer o fatorial.
+
+# Resumo:
+#
+# Uma função recursiva em Python é uma função que se chama a si mesma para resolver um problema. 
+# Ela permite a resolução de problemas complexos, dividindo-os em tarefas menores e resolvendo cada uma delas por meio de uma chamada recursiva. 
+# A função recursiva é composta por dois elementos principais: o caso base, que é a condição de parada da recursão, e o caso geral, que é a chamada recursiva que permite a resolução do problema de forma iterativa.
+# É importante ter cuidado ao usar funções recursivas, pois elas podem levar a problemas de desempenho e estouro de pilha de recursão se não forem implementadas corretamente.
