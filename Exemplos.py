cb254e051a52420ca1de4375f1a9ecb63b343e52
PedroSimbera <pedrosimbera@hotmail.com>
2023-05-08 12:08:30 -0300

    arquivo para exemplos

---
+++ Exemplos.py
@@ -0,0 +1,25 @@
+#Maioridade
+
+# def maioridade(x):
+#     n = x
+#     if n < 18:
+#         return "Menor de idade"
+#     else:
+#         return "Maior de idade"
+    
+# print(maioridade(int(input("Qual a sua idade: "))))
+
+
+
+
+# Contagem
+
+# def contagem():
+#     for i in range(1, 11):
+#         print(i)
+#     for i in range(10, 0, -1):
+#         print(i)
+
+# contagem()
+
+# O primeiro valor do "FOR" dentro do "in range" significa o início, o valor seguinte é o fim, na aparência de um terceiro valor, trata-se do incremento.
