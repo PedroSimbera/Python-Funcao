0369950be83cb4e4b4c0b3142ce14872ae281cb0
PedroSimbera <pedrosimbera@hotmail.com>
2023-05-08 11:16:51 -0300

    sobre funções

--- SobreFuncao.py
+++ SobreFuncao.py
@@ -1,3 +1,28 @@
 # Uma função em Python é um bloco de código que realiza uma tarefa específica e pode ser reutilizado em diferentes partes de um programa. Essa tarefa pode incluir a execução de cálculos, operações de entrada e saída, processamento de dados e muito mais. Uma função é definida com a palavra-chave "def", seguida do nome da função, parâmetros de entrada entre parênteses e, opcionalmente, uma documentação da função, que descreve o que a função faz e quais são os parâmetros de entrada e saída. A função pode retornar um valor para o código que a chamou ou simplesmente realizar uma ação sem retorno de valor. Usando funções em Python, o código pode ser organizado, reutilizado e mantido de forma mais eficiente, facilitando a leitura e a depuração.
 
-#É feita o código e dentro da parte que precisa utilizar a função é feito tão somente uma chamada.
+#É feita o código, dentro do prórprio "arquivo", como também pode ser feito em outro. Com isso, na parte que desejar utilizar a função, irá realizar o chamamento.
+
+
+#Em python a função é iniciada com "def" - define(ir)  a função.
+#Sem seguida se dará o "nome" da função, seguida de parênteses ":"
+
+#Exemplo:
+
+# def soma():
+#   print("Imprimindo a função")
+
+#Para chamar basta:
+
+#soma()
+
+#Ao executar o código, somente a linha 16 será executada, e irá ser feito o print com aquela saída. Assim, não precisaria escrever novamente o print.
+
+#Utilizando variáveis e return na função:
+
+# --- def soma():
+#       txt = "imprimindo o resultado com variável"
+#       return txt
+
+# Para imprimir: print(soma())
+
+#Neste caso o return está retornando o valor da variável para função que foi definida, só que o print só acontecerá neste caso se estiver printando a função.
